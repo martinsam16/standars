150aa750abc48e55be41b802ef3aff0dc1d2f31b
martinsam16 <martin.saman@vallegrande.edu.pe>
2021-03-16 23:13:54 -0500

    feat(spring-modularized): initialize

---
+++ spring-modulizared-generate/README.md
@@ -0,0 +1,3 @@
+`
+mvn archetype:generate -DgroupId=com.martinsam -DartifactId=spring-demo -DarchetypeGroupId=com.martinsam -DarchetypeArtifactId=spring-modulizared-generate
+`
---
+++ spring-modulizared-generate/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $package;
+
+/**
+ * Replace for SpringBoot application!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
